3531e3a5418310a501734ea7cccc1831636127ec
Michael Probber <michael@probber.com>
2018-12-07 21:06:47 -0800

    Use helmet, move static publish pages to new repo

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Root from './Root';
 
-const rootNode = document.querySelector('#root');
-
-if (rootNode) {
-  ReactDom.render(<Root />, rootNode);
-} else {
-  throw new Error('Could not find root node');
-}
+const rootNode = document.createElement('div');
+if (document.body) document.body.appendChild(rootNode);
+ReactDom.render(<Root />, rootNode);
